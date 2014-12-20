e27e097908486a82a2f01d699ab3223e6df8f3d2
wangrenhui <wangrenhui1990@gmail.com>
2014-12-20 16:57:34 +0800

    route  in  use

--- restj-example/src/main/java/cn/dreampie/TestController.java
+++ restj-example/src/main/java/cn/dreampie/TestController.java
@@ -8,7 +8,7 @@ import cn.dreampie.route.Controller;
  * Created by ice on 14-12-19.
  */
 public class TestController extends Controller {
-  @GET("/tests/:name")
+  @GET("/tests/{name}")//@GET("/tests/:name")  两种格式
   public String find(String name) {
     return "/tests/" + name;
   }
